4a78d688e14c9988b7ea373cebced1306527aad0
Jiayi Xin <jiayi920429@gmail.com>
2021-09-20 14:37:31 -0700

    add rollup js to generate bundle

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import { babel } from '@rollup/plugin-babel';
+
+const config = {
+  input: 'src/icon/index.js',
+  output: {
+    file: 'dist/index.esm.js',
+    format: 'esm',
+  },
+  external: [/@babel\/runtime/, 'react'],
+  plugins: [
+    babel({
+      babelHelpers: 'runtime',
+      plugins: ['@babel/plugin-transform-runtime'],
+    }),
+  ],
+};
+
+export default config;
