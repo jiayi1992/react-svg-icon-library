d20396d8f360a8df3df1be9ac30fb5d72609edb0
Jiayi Xin <jiayi920429@gmail.com>
2021-09-21 11:35:03 -0700

    install published package and update readme

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import { Sushi } from 'react-svg-icon-library';
+import { Sushi as LocalSushi } from 'react-svg-icon-library';
+import { Sushi as OnlineSushi } from '@jiayi920429/react-svg-icon-library';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Sushi style={{ width: '200px' }} />
-        <p>The demo of using svg icon</p>
+        <LocalSushi style={{ width: '200px' }} />
+        <p>This is the demo of using local package</p>
+        <OnlineSushi style={{ width: '200px' }} />
+        <p>This is the demo of using published package</p>
         <a
           className="App-link"
           href="https://github.com/jiayi1992/react-svg-icon-library"
